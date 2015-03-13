0371dac57bec8713f3e28409107b410736890884
waviq subhi <waviq.subkhi@gmail.com>
2015-03-14 01:32:11 +0700

    route update

--- Laravel4/app/routes.php
+++ Laravel4/app/routes.php
@@ -1,19 +1,24 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
-Route::get('/about', function ()
-{
-	return '<h1>Halo</h1>'
-	.'Selamat Datang di web aplikasi saya <br>'
-	.'Laravel emank keren.';
+Route::get('/about', function () {
+    return '<h1>Halo</h1>'
+            . 'Selamat Datang di web aplikasi saya <br>'
+            . 'Laravel emank keren.';
+});
+
+Route::get('/testModel', function () {
+
+    $name = DB::table('customers')->where('nama', 'waviq')->pluck('nama');
+    return $name;
 });
